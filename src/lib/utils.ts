33c78e11d4c4f6da3a089da204d785cd7d39e8d8
Andre Neidert <acneidert@gmail.com>
2026-01-14 17:51:33 -0300

    teste

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import type { ClassValue } from 'clsx'
 
+//teste
 export function cn(...inputs: Array<ClassValue>) {
   return twMerge(clsx(inputs))
 }
